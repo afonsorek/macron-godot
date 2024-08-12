0bedc509ae8b7c98f256d4ae524c17d988c727fb
Rafael Teivfik MacOS <rafael.teivfik2@gmail.com>
2024-08-12 16:04:43 -0300

    Melhorias de codigo

--- views/pivotable_view.gd
+++ views/pivotable_view.gd
@@ -1,10 +1,11 @@
-extends PivotableView
-class_name IngredientView
+extends Node3D
+class_name PivotableView
 
 #region Enums
 #endregion
 
 #region Parameters (consts and exportvars)
+@export var look_at_camera := true
 #endregion
 
 #region Signals
@@ -27,18 +28,20 @@ func _ready():
 	pass
 	
 func _process(_delta):
-	super._process(_delta)
+	if visible and look_at_camera:
+		_look_at_camera()
 	
 func _physics_process(_delta):
 	pass
 #endregion
 
 #region Public functions
-func set_color(color: Color):
-	%Sprite.modulate = color
 #endregion
 
 #region Private functions
+func _look_at_camera():
+	var cam = CameraManager.camera
+	%Pivot.look_at(cam.position,Vector3.UP,true)
 #endregion
 
 #region Subclasses
