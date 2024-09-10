2dbea033ce599bd4db42884e58d607b306e68700
Isabela :) <isabastosj@gmail.com>
2024-09-10 14:37:13 -0300

    uau
    uau ele faz o negocio

--- scenes_and_controllers/cleaver_view.gd
+++ scenes_and_controllers/cleaver_view.gd
@@ -5,4 +5,7 @@ class_name CleaverClass
 func cleaver_animation():
 	%NeutralCleaver.hide()
 	%CuttingCleaver.show()
+	await get_tree().create_timer(0.1).timeout
+	%NeutralCleaver.show()
+	%CuttingCleaver.hide()
 	
