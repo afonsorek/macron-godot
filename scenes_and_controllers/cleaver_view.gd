725675ae42f3501f51f14991f66c7fbbd2216356
Isabela :) <isabastosj@gmail.com>
2024-09-10 15:15:39 -0300

    cutting animation working

--- scenes_and_controllers/cleaver_view.gd
+++ scenes_and_controllers/cleaver_view.gd
@@ -4,6 +4,9 @@ class_name CleaverClass
 
 func cleaver_animation():
 	%NeutralCleaver.hide()
+	%SmearCleaver.show()
+	await get_tree().create_timer(0.05).timeout
+	%SmearCleaver.hide()
 	%CuttingCleaver.show()
 	await get_tree().create_timer(0.1).timeout
 	%NeutralCleaver.show()
