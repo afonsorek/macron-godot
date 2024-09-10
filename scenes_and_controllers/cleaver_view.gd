9e86567c7ed6c168db5273cc6488a93ca2299330
Isabela :) <isabastosj@gmail.com>
2024-09-10 15:26:31 -0300

    improved textures and smear animation

--- scenes_and_controllers/cleaver_view.gd
+++ scenes_and_controllers/cleaver_view.gd
@@ -5,7 +5,7 @@ class_name CleaverClass
 func cleaver_animation():
 	%NeutralCleaver.hide()
 	%SmearCleaver.show()
-	await get_tree().create_timer(0.05).timeout
+	await get_tree().create_timer(0.03).timeout
 	%SmearCleaver.hide()
 	%CuttingCleaver.show()
 	await get_tree().create_timer(0.1).timeout
