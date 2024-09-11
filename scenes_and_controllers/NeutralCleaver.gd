0c2f02a6489f13c13501afc1a5775c6297fecd41
Isabela :) <isabastosj@gmail.com>
2024-09-11 15:04:54 -0300

    animation fixes for resolution

--- scenes_and_controllers/NeutralCleaver.gd
+++ scenes_and_controllers/NeutralCleaver.gd
@@ -7,6 +7,7 @@ extends Sprite2D
 @export var closer_size = Vector2(0.541, 0.541)
 
 func _move_closer():
+	show()
 	var tween : Tween = get_tree().create_tween()
 	tween.tween_property(self, "global_position", closer_position, animation_speed).set_trans(Tween.TRANS_SINE)
 	tween.parallel().tween_property(self, "scale", closer_size, animation_speed).set_trans(Tween.TRANS_SINE)
@@ -15,4 +16,4 @@ func _move_farder():
 	var tween : Tween = get_tree().create_tween()
 	tween.tween_property(self, "global_position", farder_position, animation_speed).set_trans(Tween.TRANS_SINE)
 	tween.parallel().tween_property(self, "scale", farder_size, animation_speed).set_trans(Tween.TRANS_SINE)
-	
+	tween.tween_callback(func(): hide())
