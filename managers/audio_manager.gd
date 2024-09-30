edccba10726f0b2687390fc40b170936de0f9dd0
Isabela :) <isabastosj@gmail.com>
2024-09-30 16:29:42 -0300

    randomized sounds

--- managers/audio_manager.gd
+++ managers/audio_manager.gd
@@ -1,13 +1,11 @@
 extends Node
 class_name AudioManagerSFX
 
-@onready var dry_cut_1 = $DryCut1
-@onready var dry_cut_2 = load("res://assets/sfx/cutting_sounds/cut-dry-2.wav")
-@onready var dry_cut_3 = load("res://assets/sfx/cutting_sounds/cut-dry-3.wav")
+@onready var dry_cutting_sounds = $DryCuttingSounds
 
 var player : AudioStreamPlayer
 
 	
 	
 func play_dry_cutting():
-	dry_cut_1.play()
+	dry_cutting_sounds.play()
