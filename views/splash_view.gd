9c1111987998b6d20bf2b6e0bb6c57d2154f8d96
Rafael Teivfik MacOS <rafael.teivfik2@gmail.com>
2024-10-07 16:30:35 -0300

    Generic particles

--- views/splash_view.gd
+++ views/splash_view.gd
@@ -1,12 +1,23 @@
-extends Sprite3D
+extends PivotableView
 class_name SplashView
 
 @export var animation_speed = 0.3
-@export var bigger_size = Vector3(2.5, 2.5, 2.5)
+@export var smaller_size := 1.25
+@export var bigger_size := 2.5
+
+@onready var sprite : Sprite3D = %Sprite
 	
 func animate_splash():
+	_set_random_rotation()
 	show()
-	scale = Vector3.ZERO
+	sprite.scale = Vector3.ZERO
 	var tween : Tween = get_tree().create_tween()
-	tween.tween_property(self, "scale", bigger_size, animation_speed).set_trans(Tween.TRANS_SINE)
-	tween.tween_callback(func(): hide())
+	var random_scale = randf_range(smaller_size,bigger_size)
+	tween.tween_property(sprite, "scale", Vector3(random_scale,random_scale,random_scale), animation_speed).set_trans(Tween.TRANS_SINE)
+	tween.tween_callback(hide)
+	
+func set_modulate(new_color : Color):
+	sprite.modulate = new_color
+	
+func _set_random_rotation():
+	sprite.rotation = Vector3(0.0,0.0,randf_range(0.0,2*PI))
