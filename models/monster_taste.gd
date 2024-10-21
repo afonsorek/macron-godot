e402dc7bdbbd0e611dbf0890df763b0447de3e8b
Rafael Teivfik MacOS <rafael.teivfik2@gmail.com>
2024-10-21 18:24:39 -0300

    Por algum motivo o monster_taste.gd nao foi?

---
+++ models/monster_taste.gd
@@ -0,0 +1,24 @@
+class_name MonsterTaste
+extends Resource
+
+#region Enums
+#endregion
+
+#region Variables
+@export var element : Global.Element
+@export var likes : bool
+#endregion
+
+#region Computed properties
+#endregion
+
+func _init():
+	pass
+
+#region Public functions
+#endregion
+
+#region Subclasses
+#endregion
+
+
